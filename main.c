96641e10e91a1a5c12dc5f0f57a2e6649b775bd0
quentin <quentin@minster.io>
2021-12-09 07:03:13 +0000

    add format string vuln in status handler, "exploit" code, fix some stuff

--- main.c
+++ main.c
@@ -7,6 +7,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define UNUSED __attribute__((unused))
+
 /* Force a compilation error if condition is true, but also produce a
    result (of value 0 and type size_t), so the expression can be used
    e.g. in a structure initializer (or where-ever else comma expressions
@@ -17,6 +19,7 @@
 #define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]) + MUST_BE_ARRAY(arr))
 
 #define RED "\e[1;31m"
+#define GREEN "\e[1;32m"
 #define BLUE "\e[1;34m"
 #define BLACK "\e[0m"
 
@@ -57,8 +60,9 @@ union thing_data {
         unsigned strength;
     } monster;
     struct chest {
-        char *contents;
+        const char *name;
         unsigned lock;
+        char *contents;
     } chest;
 };
 
@@ -74,7 +78,7 @@ struct thing {
 #define ECW_FLAG "ECW{flag}"
 static struct thing flag;
 
-static struct thing *universe[1024] = {&flag, 0};
+static struct thing *universe[1024] = {&flag, NULL};
 
 unsigned reserve_thing()
 {
@@ -119,7 +123,7 @@ void delete_thing(void *data)
         reply_error("bad id");
         return;
     }
-    
+
     free(universe[i]);
     universe[i] = NULL;
 }
@@ -181,6 +185,11 @@ bool parse_player_with_args(char **args, struct player **player, const char *fmt
 
 void cmd_player(char *name)
 {
+    if (name == NULL || strlen(name) < 1) {
+        reply_error("bad name");
+        return;
+    }
+
     unsigned i = reserve_thing();
     if (i == 0)
         return;
@@ -256,11 +265,12 @@ void cmd_chest(char *args)
     }
 
     c->type = CHEST;
-    c->d.chest.contents = strdup(contents);
+    c->d.chest.name = "chest";
     c->d.chest.lock = lock;
+    c->d.chest.contents = strdup(contents);
     universe[i] = c;
 
-    reply_msg("there's a chest in the room");
+    reply_msg("there's a %s in the room", c->d.chest.name);
     reply("id", "%zu", i);
 }
 
@@ -277,7 +287,7 @@ void cmd_attack(char *args)
     reply_msg("%A", p, &m->d.monster);
 }
 
-int printf_attack(FILE *stream, __attribute__((unused)) const struct printf_info *info, const void *const *args)
+int printf_attack(FILE *stream, UNUSED const struct printf_info *info, const void *const *args)
 {
     struct player *p = *((struct player**)(args[0]));
     struct monster *m = *((struct monster**)(args[1]));
@@ -290,7 +300,7 @@ int printf_attack(FILE *stream, __attribute__((unused)) const struct printf_info
 
     int written = 0;
     written += fprintf(stream, "%s hits %s for " BLUE "%d" BLACK " damage", p->name, m->name, p->level);
-    written += fprintf(stream, "\n %s hits %s for " RED "%d" BLACK " damage", m->name, p->name, attack);
+    written += fprintf(stream, "\n %s hits %s for " GREEN "%d" BLACK " damage", m->name, p->name, attack);
     if (m->hitpoints <= 0) {
         written += fprintf(stream, "\n %s dies!", m->name);
         if (gain_experience(p, m->strength))
@@ -311,14 +321,14 @@ int printf_attack(FILE *stream, __attribute__((unused)) const struct printf_info
     return written;
 }
 
-int printf_arginfo_attack(__attribute__((unused)) const struct printf_info *info, __attribute__((unused)) size_t n, __attribute__((unused)) int *argtypes, __attribute__((unused)) int *sizes)
+int printf_arginfo_attack(UNUSED const struct printf_info *info, UNUSED size_t n, UNUSED int *argtypes, UNUSED int *sizes)
 {
     if (n >= 2) {
-		argtypes[0] = PA_POINTER;
-		sizes[0] = sizeof(struct player*);
-		argtypes[1] = PA_POINTER;
-		sizes[1] = sizeof(struct monster*);
-	}	
+        argtypes[0] = PA_POINTER;
+        sizes[0] = sizeof(struct player*);
+        argtypes[1] = PA_POINTER;
+        sizes[1] = sizeof(struct monster*);
+    }
     return 2;
 }
 
@@ -332,7 +342,7 @@ void cmd_heal(char *args)
     reply_msg("%H", p, hp);
 }
 
-int printf_heal(FILE *stream, __attribute__((unused)) const struct printf_info *info, const void *const *args)
+int printf_heal(FILE *stream, UNUSED const struct printf_info *info, const void *const *args)
 {
     struct player *p = *((struct player**)(args[0]));
     int hp = *((int*)(args[1]));
@@ -344,14 +354,14 @@ int printf_heal(FILE *stream, __attribute__((unused)) const struct printf_info *
     return written;
 }
 
-int printf_arginfo_heal(__attribute__((unused)) const struct printf_info *info, __attribute__((unused)) size_t n, __attribute__((unused)) int *argtypes, __attribute__((unused)) int *sizes)
+int printf_arginfo_heal(UNUSED const struct printf_info *info, UNUSED size_t n, UNUSED int *argtypes, UNUSED int *sizes)
 {
     if (n >= 2) {
-		argtypes[0] = PA_POINTER;
-		sizes[0] = sizeof(struct player*);
-		argtypes[1] = PA_INT;
-		sizes[1] = sizeof(int);
-	}	
+        argtypes[0] = PA_POINTER;
+        sizes[0] = sizeof(struct player*);
+        argtypes[1] = PA_INT;
+        sizes[1] = sizeof(int);
+    }
     return 2;
 }
 
@@ -369,37 +379,38 @@ void cmd_open(char *args)
     reply_msg("%O", p, &c->d.chest);
 }
 
-int printf_open(FILE *stream, __attribute__((unused)) const struct printf_info *info, const void *const *args)
+int printf_open(FILE *stream, UNUSED const struct printf_info *info, const void *const *args)
 {
     struct player *p = *((struct player**)(args[0]));
     struct chest *c = *((struct chest**)(args[1]));
 
     int written = 0;
-    if (p->level >= c->lock) {
-        written += fprintf(stream, "lock picked successfully!");
+    written += fprintf(stream, "%s attempts to open a %s", p->name, c->name);
+    if ((int) p->level >= (int) c->lock) {
+        written += fprintf(stream, "\n lock picked successfully!");
         if (p->level == c->lock && gain_experience(p, 1))
             written += fprintf(stream, "\n %s levels up", p->name);
         if (strlen(c->contents) > 0)
             written += fprintf(stream, "\n %s finds %s", p->name, c->contents);
         else
-            written += fprintf(stream, "\n the chest is empty :(");
+            written += fprintf(stream, "\n it's empty :(");
         if (c != &flag.d.chest) {
             free(c->contents);
             delete_thing(c);
         }
     } else
-        written += fprintf(stream, "this lock is too tough");
+        written += fprintf(stream, "\n this lock is too tough");
     return written;
 }
 
-int printf_arginfo_open(__attribute__((unused)) const struct printf_info *info, __attribute__((unused)) size_t n, __attribute__((unused)) int *argtypes, __attribute__((unused)) int *sizes)
+int printf_arginfo_open(UNUSED const struct printf_info *info, UNUSED size_t n, UNUSED int *argtypes, UNUSED int *sizes)
 {
     if (n >= 2) {
-		argtypes[0] = PA_POINTER;
-		sizes[0] = sizeof(struct player*);
-		argtypes[1] = PA_POINTER;
-		sizes[1] = sizeof(struct chest*);
-	}	
+        argtypes[0] = PA_POINTER;
+        sizes[0] = sizeof(struct player*);
+        argtypes[1] = PA_POINTER;
+        sizes[1] = sizeof(struct chest*);
+    }
     return 2;
 }
 
@@ -412,7 +423,7 @@ void cmd_talk(char *args)
     reply_msg("%T", p, args);
 }
 
-int printf_talk(FILE *stream, __attribute__((unused)) const struct printf_info *info, const void *const *args)
+int printf_talk(FILE *stream, UNUSED const struct printf_info *info, const void *const *args)
 {
     const struct player *p = *((const struct player**)(args[0]));
     char *msg = *((char**)(args[1]));
@@ -421,51 +432,105 @@ int printf_talk(FILE *stream, __attribute__((unused)) const struct printf_info *
 
     if (len > 2 && msg[0] == '*' && msg[len-1] == '*') {
         msg[len-1] = '\0';
-        written += fprintf(stream, "%s " RED "%s" BLACK, p->name, &msg[1]);
+        written += fprintf(stream, "%s " GREEN "%s" BLACK, p->name, &msg[1]);
     } else
-        written += fprintf(stream, "%s says: " RED "%s" BLACK, p->name, msg);
+        written += fprintf(stream, "%s says: " GREEN "%s" BLACK, p->name, msg);
     return written;
 }
 
-int printf_arginfo_talk(__attribute__((unused)) const struct printf_info *info, __attribute__((unused)) size_t n, __attribute__((unused)) int *argtypes, __attribute__((unused)) int *sizes)
+int printf_arginfo_talk(UNUSED const struct printf_info *info, UNUSED size_t n, UNUSED int *argtypes, UNUSED int *sizes)
 {
     if (n >= 2) {
-		argtypes[0] = PA_POINTER;
-		sizes[0] = sizeof(struct player*);
-		argtypes[1] = PA_POINTER;
-		sizes[1] = sizeof(char*);
-	}	
+        argtypes[0] = PA_POINTER;
+        sizes[0] = sizeof(struct player*);
+        argtypes[1] = PA_POINTER;
+        sizes[1] = sizeof(char*);
+    }
     return 2;
 }
 
 void cmd_status(char *args)
 {
-    struct player *p;
-    if (!parse_player(&args, &p))
-        return;
+    /* parse ids */
+    unsigned ids[10];
+    int read = 0;
+    char *curarg = args;
+    size_t n;
+    for(n = 0; n < ARRAY_SIZE(ids) && sscanf(curarg, "%u%n", &ids[n], &read) == 1; n++, curarg += read);
 
-    /* TODO: print several objects so that the format string vuln when printing the player name can be used to modify subsequent objects? */
-    reply_msg("%S", p);
+    /* find things */
+    struct thing *things[10];
+    for (size_t i = 0; i < n; i++) {
+        things[i] = get_thing(ids[i]);
+        if (things[i] == NULL)
+            return;
+    }
+
+    /* print things */
+    reply_msg("%*S", n, things);
 }
 
-int printf_status(FILE *stream, __attribute__((unused)) const struct printf_info *info, const void *const *args)
+int printf_status(FILE *stream, UNUSED const struct printf_info *info, const void *const *args)
 {
-    const struct player *p = *((const struct player**)(args[0]));
+    const struct thing **things = *((const struct thing***)(args[0]));
+    int n = info->width;
     int written = 0;
-    written += fprintf(stream, p->name, p);
-    written += fprintf(stream, "\n hitpoints: " BLUE "%d" BLACK, p->hitpoints);
-    written += fprintf(stream, "\n level: " BLUE "%u" BLACK, p->level);
-    written += fprintf(stream, "\n experience: " BLUE "%d" BLACK, p->experience);
+    for (int i = 0; i < n; i++) {
+        if (i > 0)
+            written += fprintf(stream, "\n\n ");
+        const struct thing *t = things[i];
+        VERBOSE_PRINT("[t=%p d=%p]", t, &t->d);
+        switch(t->type) {
+            case PLAYER: {
+                const struct player *p = &t->d.player;
+                written += fprintf(stream, p->name, &things[i+1]->d, &things[i+2]->d, &things[i+3]->d, &things[i+4]->d); /* artificial but heh */
+                written += fprintf(stream, "\n  hitpoints: " BLUE "%d" BLACK, p->hitpoints);
+                written += fprintf(stream, "\n  level: " BLUE "%u" BLACK, p->level);
+                written += fprintf(stream, "\n  experience: " BLUE "%d" BLACK, p->experience);
+                break;
+            }
+
+            case MONSTER: {
+                const struct monster *m = &t->d.monster;
+                written += fprintf(stream, m->name, &things[i+1]->d, &things[i+2]->d, &things[i+3]->d, &things[i+4]->d); /* artificial but heh */
+                written += fprintf(stream, "\n  hitpoints: " BLUE "%d" BLACK, m->hitpoints);
+                written += fprintf(stream, "\n  strength: " BLUE "%u" BLACK, m->strength);
+                break;
+            }
+
+            case CHEST: {
+                const struct chest *c = &t->d.chest;
+                written += fprintf(stream, "a %s", c->name);
+                written += fprintf(stream, "\n  lock: " BLUE "%d" BLACK, c->lock);
+                VERBOSE_PRINT("\n  [contents %p: <<<%s>>>]", c->contents, c->contents);
+                break;
+            }
+
+            default:
+                written += fprintf(stream, "invalid thing type, should never happen!");
+                break;
+        }
+    }
     return written;
 }
 
-int printf_arginfo_status(__attribute__((unused)) const struct printf_info *info, __attribute__((unused)) size_t n, __attribute__((unused)) int *argtypes, __attribute__((unused)) int *sizes)
+int printf_arginfo_status(UNUSED const struct printf_info *info, UNUSED size_t n, UNUSED int *argtypes, UNUSED int *sizes)
 {
     if (n >= 1) {
-		argtypes[0] = PA_POINTER;
-		sizes[0] = sizeof(struct player*);
-	}	
+        argtypes[0] = PA_POINTER;
+        sizes[0] = sizeof(struct thing*);
+    }
     return 1;
+}
+
+void cmd_gm(char *msg)
+{
+    if (msg == NULL || strlen(msg) < 1) {
+        reply_error("bad message");
+        return;
+    }
+
+    reply("gm", RED "%s" BLACK, msg);
 }
 
 static const struct {
@@ -480,19 +545,20 @@ static const struct {
     {"open", cmd_open}, // %O
     {"talk", cmd_talk}, // %T
     {"status", cmd_status}, // %S
+    {"gm", cmd_gm},
 };
 
-int printguard_n_specifier(FILE *stream, __attribute__((unused)) const struct printf_info *info, __attribute__((unused)) const void *const *args)
+int printguard_n_specifier(FILE *stream, UNUSED const struct printf_info *info, UNUSED const void *const *args)
 {
     return fprintf(stream, "<PrintGuard™: %%n deflected>");
 }
 
-int printguard_n_specifier_info(__attribute__((unused)) const struct printf_info *info, __attribute__((unused)) size_t n, __attribute__((unused)) int *argtypes, __attribute__((unused)) int *size)
+int printguard_n_specifier_info(UNUSED const struct printf_info *info, UNUSED size_t n, UNUSED int *argtypes, UNUSED int *size)
 {
     return 1; /* TODO: consume 0 args? */
 }
 
-int main(__attribute__((unused)) int argc, __attribute__((unused)) char *argv[])
+int main(UNUSED int argc, UNUSED char *argv[])
 {
     if (register_printf_specifier('A', printf_attack, printf_arginfo_attack) ||
         register_printf_specifier('H', printf_heal, printf_arginfo_heal) ||
@@ -505,8 +571,9 @@ int main(__attribute__((unused)) int argc, __attribute__((unused)) char *argv[])
     }
 
     flag.type = CHEST;
-    flag.d.chest.contents = "the flag: " ECW_FLAG;
+    flag.d.chest.name = "golden chest";
     flag.d.chest.lock = LEVEL_CAP + 1; /* out of reach for fair lock picking */
+    flag.d.chest.contents = "the flag: " ECW_FLAG;
 
     while (!feof(stdin) && !ferror(stdin)) {
         char buf[1024];
@@ -523,6 +590,8 @@ int main(__attribute__((unused)) int argc, __attribute__((unused)) char *argv[])
             char *cmdctx;
             char *cmd = strtok_r(line, " ", &cmdctx);
             if (cmd == NULL) /* TODO: error? */
+                continue;
+            if (cmd[0] == '#')
                 continue;
             char *arg = strtok_r(NULL, "", &cmdctx);
 
